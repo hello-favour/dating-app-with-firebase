ae738a764ee635b578a4d85b4548100721393142
Favour <hello.williamsfav@gmail.com>
2023-05-22 07:32:57 +0100

    theming project

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFA42823);
+  static const Color lowQualityPink = Color(0xFFEB5A5A);
+  static const Color secondaryColor = Color(0xFFDADADA);
+}
