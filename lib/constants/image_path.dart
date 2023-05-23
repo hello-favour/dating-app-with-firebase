e342bd0181c5d432693f69b583a65f6137c8c42b
Favour <hello.williamsfav@gmail.com>
2023-05-23 01:02:58 +0100

    set up assets images and font

---
+++ lib/constants/image_path.dart
@@ -0,0 +1,6 @@
+class ImagePath {
+  static const logoImage = "assets/images/logo.png";
+  static const connectImage = "assets/images/connect.png";
+  static const findImage = "assets/images/find.png";
+  static const makeFriendsImage = "assets/images/make_friends.png";
+}
