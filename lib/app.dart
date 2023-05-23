be1dd934bd994336b77233089c9ed1d5802b1d55
Favour <hello.williamsfav@gmail.com>
2023-05-23 01:04:03 +0100

    name app title

--- lib/app.dart
+++ lib/app.dart
@@ -6,8 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dating App',
       theme: ThemeData(
+        fontFamily: "Poppins",
         primarySwatch: Colors.blue,
       ),
     );
